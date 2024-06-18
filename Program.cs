30f0288db97595fb652d4e73fe0579efdeedcc26
Chosamee <cytor@naver.com>
2024-06-18 23:02:28 +0900

    initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ImageMoji
+{
+    static class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Test Hello!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+### project
+
+<br/>
+
+use emoji by mapped image
+
+### setup
+
+<br/>
+tutorial
+
+https://learn.microsoft.com/ko-kr/dotnet/core/tutorials/with-visual-studio-code?pivots=dotnet-8-0
+
+<br/>
+
+`dotnet new console --framework net8.0 --use-program-main`
